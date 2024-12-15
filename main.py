c6c85eaab91ffabe87cb56e0856e0a4137060211
yashvithaT <148280511+yashvithaT@users.noreply.github.com>
2024-12-14 20:16:49 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-
 #   a117_traversing_turtles.py
 #   Add code to make turtles move in a circle and change colors.
 import turtle as trtl
 
 # create an empty list of turtles
 my_turtles = []
-trtl.speed = 1
+trtl.speed = 0
 
 # use interesting shapes and colors
-turtle_shapes = ["arrow", "turtle", "circle", "square", "triangle", "classic"]
-turtle_colors = ["red", "blue", "green", "orange", "purple", "gold"]
+turtle_shapes = ["arrow", "turtle", "circle", "square", "triangle", "classic", "arrow", "turtle", "circle", "square", "turtle", "arrow", "classic", "triangle"]
+turtle_colors = ["red", "blue", "green", "orange", "purple", "gold", "pink", "magenta", "turquoise", "rosy brown", "olive", "light blue", "cornflower blue", "lavender"]
+# create a list of turtles
 
 for s in turtle_shapes:
   t = trtl.Turtle(shape=s)
@@ -20,20 +20,22 @@ for s in turtle_shapes:
 #  
 startx = 0
 starty = 0
-startPos = -45
+startPos = 90
+forwardlength = 50
 
 #
 for t in my_turtles:
   t.goto(startx, starty)
   t.pendown()
-  
-  t.right(startPos)     
-  t.forward(50)
+  t.setheading(startPos)
+  t.right(45)     
+  t.forward(forwardlength)
 
 #	
   startx = t.xcor()
-  starty =t.ycor()
-  startPos += 45
+  starty = t.ycor()
+  startPos = t.heading()
+  forwardlength += 5
   
 
 wn = trtl.Screen()
